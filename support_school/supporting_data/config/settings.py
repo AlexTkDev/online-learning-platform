2646eba6844a41cfc0e3bd07bcf580a51f4c5881
alex <a8696tk@gmail.com>
2024-07-03 13:41:30 +0300

    added timezone LANGUAGE_CODE = "Europe/Kyiv"

--- support_school/supporting_data/config/settings.py
+++ support_school/supporting_data/config/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "Europe/Kyiv"
 
 TIME_ZONE = "UTC"
 
