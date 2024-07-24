e814f8a0d6d141ee908dfe68f154d8b76ece270b
alex <a8696tk@gmail.com>
2024-07-24 14:50:11 +0300

    Fix field author

--- blog_school/blog_data/blog/models.py
+++ blog_school/blog_data/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    author = models.ForeignKey('User', on_delete=models.DO_NOTHING),
+    author = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
