24eff1ff44435d161635c4fa5c5a890698a1bb7a
alex <a8696tk@gmail.com>
2024-07-30 12:14:26 +0300

    Added serializer to model Grades

--- online_school/school/test_and_grade/serializers.py
+++ online_school/school/test_and_grade/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Course
+from .models import Course, Grades
 
 
 class CourseSerializer(serializers.ModelSerializer):
@@ -7,3 +7,9 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = '__all__'
         read_only_fields = ['id']
+
+
+class GradesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Grades
+        fields = '__all__'
--- online_school/school/test_and_grade/views.py
+++ online_school/school/test_and_grade/views.py
@@ -1,23 +1,38 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
-from courses.models import Course
+from .models import Tests, Grades
 from courses.serializers import CourseSerializer
+from .serializers import GradesSerializer
+
+
+# Tests =================================================================
 
 
 class CourseListAPIView(generics.ListAPIView):
-    queryset = Course.objects.all()
+    queryset = Tests.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated]
 
 
 class CourseDetailAPIView(generics.ListCreateAPIView):
-    queryset = Course.objects.all()
+    queryset = Tests.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated]
 
 
 class CourseRetriveUpdateDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Course.objects.all()
+    queryset = Tests.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated]
+
+
+# Grades =================================================================
+
+
+class GradeListAPIView(generics.ListAPIView):
+    queryset = Grades.objects.all()
+    serializer_class = GradesSerializer
+    permission_classes = [IsAuthenticated]
+
+
