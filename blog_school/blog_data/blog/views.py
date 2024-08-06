7ca3833570591741960084b7042f11e517a1342c
alex <a8696tk@gmail.com>
2024-08-06 14:46:39 +0300

    delete permissions

--- blog_school/blog_data/blog/views.py
+++ blog_school/blog_data/blog/views.py
@@ -1,21 +1,18 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from blog.models import Post
 from blog.serializers import PostSerializer
 
 
 class PostList(generics.ListAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
 
 class CreatePost(generics.CreateAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
