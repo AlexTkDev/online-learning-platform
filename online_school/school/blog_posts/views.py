2372f7a01a2e3c5d02cfa40f89b70aaf78eb5d3f
alex <a8696tk@gmail.com>
2024-08-06 15:08:44 +0300

    fixed blog-posts

--- online_school/school/blog_posts/views.py
+++ online_school/school/blog_posts/views.py
@@ -1,11 +1,25 @@
+import requests
 from django.shortcuts import render
 from django.views.generic import View
 
 
 class PostListView(View):
-    def get(self, request):
-        context = {
-            "title": "Blog Posts",
-        }
-        return render(request, "online_school/index.html", context)
+    http_method_names = ['get', 'post']
+    template_name = 'blog_posts/post_list.html'
+    context_object_name = 'post_list'
 
+    def get(self, request, *args, **kwargs):
+        api_url = 'http://localhost:9000/api/blog/'
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()  # Генерируем исключение для HTTP ошибок
+            posts = response.json()
+        except requests.exceptions.RequestException as e:
+            posts = []
+            print(f"Error fetching data from API: {e}")
+
+        context = {
+            'post_list': posts,
+            'title': 'Blog Posts',
+        }
+        return render(request, self.template_name, context)
--- online_school/school/test_and_grade/admin.py
+++ online_school/school/test_and_grade/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Tests, Grades
+
 # Register your models here.
 
 
-admin.site.register(Course)
+admin.site.register(Tests)
+admin.site.register(Grades)
