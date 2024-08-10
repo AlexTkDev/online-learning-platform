23ab3b9dc9f7df25d83a2278290d151b28fe8551
alex <a8696tk@gmail.com>
2024-08-10 19:15:11 +0300

    feat: added user role and applied to User model

--- online_school/school/users/enums.py
+++ online_school/school/users/enums.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class Role(Enum):
-    Teacher = 'Teacher',
-    Student = 'Student',
+    Teacher = 'Teacher'
+    Student = 'Student'
     Admin = 'Admin'
--- online_school/school/users/models.py
+++ online_school/school/users/models.py
@@ -1,15 +1,17 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from .enums import Role
+from django.contrib.auth.models import AbstractUser
+from users.enums import Role
 
 
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
     first_name = models.CharField(max_length=50, blank=True, null=True)
-    role = models.CharField(max_length=50, choices=[
-        (tag.name, tag.value) for tag in Role]
-                            )  # teacher, admin or student
+    role = models.CharField(
+        max_length=50,
+        choices=[(role.value, role.value) for role in Role],
+        default=Role.Student.value
+    )
     email = models.EmailField()
     password = models.CharField(max_length=120)
 
