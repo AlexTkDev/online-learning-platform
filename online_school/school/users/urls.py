2b5cbd43e18023aa2f5ce1c90b792f788847dccb
alex <a8696tk@gmail.com>
2024-08-13 14:49:04 +0300

    feat: added path to 'logout', 'register', 'login'

--- online_school/school/users/urls.py
+++ online_school/school/users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from users.views import GetUserList, RetriveUpdateDestroyUser
+from users.views import (GetUserList,
+                         RetriveUpdateDestroyUser,
+                         LogoutUser,
+                         RegisterUser,
+                         LoginUserView)
 
 urlpatterns = [
     path('user-list/', GetUserList.as_view(), name='user-list'),
     path('user/<int:pk>/', RetriveUpdateDestroyUser.as_view(), name='user-detail'),
+    path('logout/', LogoutUser.as_view(), name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUserView.as_view(), name='login'),
+
 ]
