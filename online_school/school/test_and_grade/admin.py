5ca2e591dc2ef5636a5103530e932657ccdec841
alex <a8696tk@gmail.com>
2024-08-15 17:00:32 +0300

    fixed: display only student accounts in student grade field

--- online_school/school/test_and_grade/admin.py
+++ online_school/school/test_and_grade/admin.py
@@ -1,9 +1,34 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from users.enums import Role
 
 from .models import Tests, Grades
 
-# Register your models here.
+User = get_user_model()
 
 
-admin.site.register(Tests)
-admin.site.register(Grades)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ('test_name', 'test_date', 'test_from_course')
+    list_filter = ('test_date', 'test_from_course')
+
+
+class GradesAdminForm(forms.ModelForm):
+    class Meta:
+        model = Grades
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Фильтрация студентов по роли
+        self.fields['student_name'].queryset = User.objects.filter(role=Role.Student.name)
+
+
+class GradesAdmin(admin.ModelAdmin):
+    form = GradesAdminForm
+    list_display = ('test_name', 'student_name', 'date')
+    list_filter = ('student_name', 'date')
+
+
+admin.site.register(Tests, TestAdmin)
+admin.site.register(Grades, GradesAdmin)
